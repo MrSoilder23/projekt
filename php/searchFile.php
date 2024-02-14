641aab77f01836a459ab74dc3f21738fdbee7ef3
MrSoilder23 <72629981+MrSoilder23@users.noreply.github.com>
2024-02-14 20:07:49 +0100

    Add server relate files

--- php/searchFile.php
+++ php/searchFile.php
@@ -13,6 +13,21 @@
         if(mysqli_num_rows($result) > 0) {
             $data = mysqli_fetch_row($result);
             echo json_encode($data);
+
+            
+            $sql = "SELECT id FROM file WHERE name = '$name'";
+
+            $result = mysqli_query($conn, $sql);
+            $fileId;
+
+            while($row = mysqli_fetch_array($result)) {
+                $fileId = $row['id'];
+            }
+            
+            $sql = "UPDATE file SET relates = '$fileId' WHERE id = '$id';";
+            mysqli_query($conn, $sql);
+
+
         } else {
             header('HTTP/1.1 500 Internal Server Booboo');
             header('Content-Type: application/json; charset=UTF-8');
